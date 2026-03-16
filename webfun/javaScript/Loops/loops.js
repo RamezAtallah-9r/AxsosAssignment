efb1ddb130aab43e9fe3aa28561d6baf5309c7de
Ramez <ramez.atallah@hotmail.com>
2026-03-16 15:20:49 +0200

    fizz

---
+++ webfun/javaScript/Loops/loops.js
@@ -0,0 +1,32 @@
+for(var i=0;i<=20;i++){
+    if(i%2==1){
+        console.log(i);
+    }
+}
+
+for(i=1;i<=100;i++){
+    if(i%3==0){
+        console.log(i);
+        
+    }
+}
+
+for(i=4;i>-4;i-=1.5){
+    console.log(i);
+    
+}
+
+var sum=0;
+for(i=1;i<=100;i++){
+    sum+=i;
+}
+console.log(sum);
+
+
+var product=1;
+for (let index = 1; index <= 12; index++) {
+    product*=index;
+}
+console.log(product);
+
+
